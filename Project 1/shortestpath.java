61ab934a8fa266c92170e1a2927ee026fdc7219e
Gabbi Forsythe <gforsythe17@gmail.com>
2019-10-23 23:35:23 -0400

    Wrote the main function and the print matrix function

--- Project 1/shortestpath.java
+++ Project 1/shortestpath.java
@@ -5,12 +5,53 @@
 */
 
 import java.io.*;
+import java.util.*;
 
 class shortestpath {
-    
+
+    // Function to print out the matrix all pretty like
+    public static void printMatrix (int n, int W[][]) {
+        for(int x=0; x<n; x++){
+            System.out.print(x+" ");
+        }
+
+        System.out.println();
+        for (int i=0; i<n; i++) {
+            System.out.print(i+": ");
+            for (int j=0; j<n; j++) {
+                System.out.print(W[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void floydAlg (int n, int [][] W, int P[][], int [][] D) {
+        
+    }
+
+
     // Main function of the program
-    public static void main (String [] args) {
-        System.out.println("hi");
+    public static void main (String [] args)
+        throws IOException
+    {
+        // Text file that numbers will be read from; The first integer in the text file represents the number of vertices in the graph; -1000 in the text file represents infinity
+        Scanner txtfile=new Scanner(new FileReader("paths.txt"));
+        
+        // first int in the file represents the number of vertices
+        int size;
+        size = txtfile.nextInt(); 
+
+        // declare the matrices for the shortest path
+        int[][] W = new int[size][size];
+        int[][] P = new int [size][size];
+        int[][] D = new int [size][size];
+
+        for(int i=0; i<size; i++) {
+            for(int j=0; j<size; j++) {
+                W[i][j] = txtfile.nextInt();
+            }
+        }
+
     }
 
 }
