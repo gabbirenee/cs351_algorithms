c58c882a8063de2a60c8ae6c5eedf3ab2dc8086a
Gabbi Forsythe <gforsythe17@gmail.com>
2019-10-23 22:57:23 -0400

    added shortestpath.java

---
+++ Project 1/shortestpath.java
@@ -0,0 +1,16 @@
+/*
+    PROJECT 1: FLOYD'S SHORTEST PATH ALGORITHM
+    This program will read in data from a text file and use Floyd's algorithm to compute the
+    shortest path matrix, for every vertex in the directed graph
+*/
+
+import java.io.*;
+
+class shortestpath {
+    
+    // Main function of the program
+    public static void main (String [] args) {
+        System.out.println("hi");
+    }
+
+}
